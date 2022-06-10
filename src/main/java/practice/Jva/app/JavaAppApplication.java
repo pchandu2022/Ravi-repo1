efdd3e8edba1ed91e578fc70f005f1d77e7de357
P.chandrashekhar <pchandu2022@gmail.com>
2022-06-10 13:34:36 +0530

    first commit

--- src/main/java/practice/Jva/app/JavaAppApplication.java
+++ src/main/java/practice/Jva/app/JavaAppApplication.java
@@ -8,6 +8,7 @@ public class JavaAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaAppApplication.class, args);
+		System.out.println("hello world");
 	}
 
 }
