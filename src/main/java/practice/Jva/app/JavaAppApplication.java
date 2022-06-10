d68fe7a6ead16910caed22ee75fa810cad6e8ab6
P.chandrashekhar <pchandu2022@gmail.com>
2022-06-10 13:22:41 +0530

    first

---
+++ src/main/java/practice/Jva/app/JavaAppApplication.java
@@ -0,0 +1,13 @@
+package practice.Jva.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAppApplication.class, args);
+	}
+
+}
